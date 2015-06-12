55f02f474b94a71d5b30a42ff128ed35c1b57e09
Wang Zhen <lazybetrayer@gmail.com>
2015-06-12 20:32:45 +0800

    update
    Signed-off-by: Wang Zhen <lazybetrayer@gmail.com>

---
+++ contains-duplicate.cc
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for (auto n : nums) {
+            if (s.count(n)) {
+                return true;
+            }
+            s.insert(n);
+        }
+        return false;
+    }
+};
